cccd4024645e97b322cabaf8b230ce0e4b81f037
enkitaa <ankkita95@gmail.com>
2021-07-20 15:59:56 +0530

    test: Initial endpoint test with supertest

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,1 +1,14 @@
 // Test file for index.ts
+
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async () => {
+        const response = await request.get(
+            '/api/images?filename=fjord&height=500&width=500'
+        );
+        expect(response.status).toBe(200);
+    });
+});
