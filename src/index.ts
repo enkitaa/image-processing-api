742776f4fcb51a2f8cf12a964f7cbe85f61dcd60
enkitaa <ankkita95@gmail.com>
2021-07-25 21:38:51 +0530

    chore: Add morgan server logger

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 
 import express from 'express';
 import routes from './routes/index';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
+app.use(morgan(':date[web] :method :url :status :response-time ms - :res[content-length]'))
 app.use('/api', routes);
 
 app.listen(port, () => {
